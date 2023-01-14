409e629d23b86175a20252ed403b178f962c1559
shirs12 <shirseg98@gmail.com>
2023-01-14 19:54:28 +0200

    initial commit

---
+++ src/test/java/com/example/SpringBootTodoApp/SpringBootTodoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBootTodoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTodoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
