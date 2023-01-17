908c0f1d199b4e468e4366b52bbe110d7a4c7c32
shirs12 <shirseg98@gmail.com>
2023-01-17 18:07:23 +0200

    updated task controller

--- src/main/java/com/example/SpringBootTodoApp/controllers/TaskController.java
+++ src/main/java/com/example/SpringBootTodoApp/controllers/TaskController.java
@@ -1,7 +1,9 @@
 package com.example.SpringBootTodoApp.controllers;
 
+import com.example.SpringBootTodoApp.repositories.TaskRepo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,10 +13,14 @@ import org.springframework.web.servlet.ModelAndView;
 public class TaskController {
     private final Logger logger = LoggerFactory.getLogger(TaskController.class);
 
+    @Autowired
+    private TaskRepo taskRepo;
+
     @GetMapping("/")
     public ModelAndView index(){
         logger.debug("request to GET index");
         ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("tasksList", taskRepo.findAll());
         return modelAndView;
     }
 
